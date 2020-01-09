6eaaa150833d73fafd29ea38d6cef1030e57a26e
Eduardo Falcão <eduardolfalcao@gmail.com>
2020-01-09 10:02:26 -0300

    Added key gen algorithm on properties

--- src/br/com/educhainwallet/setup/PropertiesManager.java
+++ src/br/com/educhainwallet/setup/PropertiesManager.java
@@ -10,6 +10,7 @@ public class PropertiesManager {
 	private static final String MEM_POOL_HOST = "mem_pool_host";
 	private static final String WALLET_SENDER = "wallet_sender";
 	private static final String WALLET_RECEIVER = "wallet_receiver";
+	private static final String KEYGEN_ALGORITHM = "keygen_algorithm";	
 	private static final String PRIV_KEY_EXTENSION = "priv_key_extension";
 	private static final String PUB_KEY_EXTENSION = "pub_key_extension";
 	
@@ -21,7 +22,6 @@ public class PropertiesManager {
 		try {
 			props.load(new FileInputStream("config.properties"));
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
@@ -34,6 +34,18 @@ public class PropertiesManager {
 		return props.getProperty(PropertiesManager.MEM_POOL_HOST);
 	}
 	
+	public String getWalletSender() {
+		return props.getProperty(PropertiesManager.WALLET_SENDER);
+	}
+	
+	public String getWalletReceiver() {
+		return props.getProperty(PropertiesManager.WALLET_RECEIVER);
+	}
+	
+	public String getKeyGenAlgorithm() {
+		return props.getProperty(PropertiesManager.KEYGEN_ALGORITHM);
+	}
+	
 	public String getPrivKeyExtension() {
 		return props.getProperty(PropertiesManager.PRIV_KEY_EXTENSION);
 	}
@@ -42,13 +54,7 @@ public class PropertiesManager {
 		return props.getProperty(PropertiesManager.PUB_KEY_EXTENSION);
 	}
 	
-	public String getWalletSender() {
-		return props.getProperty(PropertiesManager.WALLET_SENDER);
-	}
 	
-	public String getWalletReceiver() {
-		return props.getProperty(PropertiesManager.WALLET_RECEIVER);
-	}
 	
 	public static void main(String[] args) {
 		PropertiesManager prop = PropertiesManager.getInstance();
